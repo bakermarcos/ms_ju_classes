dec762aa5bd7ee368092d02d9c22bfe3d691c9f8
Marcos Baker <bakermarcos52@gmail.com>
2025-10-17 01:14:40 -0300

    Update image imports and HTML references
    Renamed the main JS asset file and updated references in index.html. Modified image imports in lib/images.ts to reflect new asset paths. Updated package-lock.json due to dependency changes.

--- lib/images.ts
+++ lib/images.ts
@@ -1,10 +1,10 @@
 // Local images served from public/images.
-export const classroomScene = "/images/classroom.png";
-export const juliaAndBooks = "/images/julia-and-books.png";
-export const juliaBookCover = "/images/julia-book.png";
-export const juliaInBookstore = "/images/julia-in-bookstore.png";
-export const juliaInBookstoreReading = "/images/julia-in-bookstore-reading.png";
-export const juliaStainedGlass = "/images/julia-vitral.png";
-export const juliaWithRobot = "/images/julia-with-robot-and-book.png";
-export const robotsTraveling = "/images/robots.png";
-export const msJuLogo = "/images/msju_logo.png";
+export const classroomScene = "./images/classroom.png";
+export const juliaAndBooks = "./images/julia-and-books.png";
+export const juliaBookCover = "./images/julia-book.png";
+export const juliaInBookstore = "./images/julia-in-bookstore.png";
+export const juliaInBookstoreReading = "./images/julia-in-bookstore-reading.png";
+export const juliaStainedGlass = "./images/julia-vitral.png";
+export const juliaWithRobot = "./images/julia-with-robot-and-book.png";
+export const robotsTraveling = "./images/robots.png";
+export const msJuLogo = "./images/msju_logo.png";
